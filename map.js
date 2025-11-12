8e5365999f751f9467b814b2bc48d52ebe148f98
Coco-Juice <tonybai321@gmail.com>
2025-11-11 21:48:08 -0800

    added circles for bike stations and traffic

--- map.js
+++ map.js
@@ -64,7 +64,34 @@ map.on('load', async () => {
   }
 
   let stations = jsonData.data.stations;
-  console.log('Stations Array:', jsonData.data);
+  const trips = await d3.csv('bluebikes-traffic-2024-03.csv')
+
+  const departures = d3.rollup(
+    trips,
+    (v) => v.length,
+    (d) => d.start_station_id,
+  );
+
+  const arrivals = d3.rollup(
+    trips,
+    (v) => v.length,
+    (d) => d.end_station_id,
+  );
+
+  stations = stations.map((station) => {
+    let id = station.short_name;
+    station.arrivals = arrivals.get(id) ?? 0;
+    // TODO departures
+    station.departures = departures.get(id) ?? 0;
+    // TODO totalTraffic
+    station.totalTraffic = station.arrivals + station.departures
+    return station;
+  });
+
+  const radiusScale = d3
+    .scaleSqrt()
+    .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+    .range([0, 25]);
 
   const svg = d3.select('#map').select('svg');
   const circles = svg
@@ -72,21 +99,30 @@ map.on('load', async () => {
     .data(stations)
     .enter()
     .append('circle')
-    .attr('r', 5) // Radius of the circle
-    .attr('fill', 'steelblue') // Circle fill color
+    .attr('r', d => radiusScale(d.totalTraffic)) // Radius of the circle
+    .attr('fill', 'red') // Circle fill color
     .attr('stroke', 'white') // Circle border color
     .attr('stroke-width', 1) // Circle border thickness
-    .attr('opacity', 0.8); // Circle opacity
+    .attr('opacity', 0.8) // Circle opacity
+    .each(function (d) {
+      // Add <title> for browser tooltips
+      d3.select(this)
+        .append('title')
+        .text(
+          `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+        );
+    });
 
-    function updatePositions() {
-      circles
-        .attr('cx', (d) => getCoords(d).cx) // Set the x-position using projected coordinates
-        .attr('cy', (d) => getCoords(d).cy); // Set the y-position using projected coordinates
-    }    
+  function updatePositions() {
+    circles
+      .attr('cx', (d) => getCoords(d).cx) // Set the x-position using projected coordinates
+      .attr('cy', (d) => getCoords(d).cy); // Set the y-position using projected coordinates
+  }    
 
-    updatePositions();
-    map.on('move', updatePositions); // Update during map movement
-    map.on('zoom', updatePositions); // Update during zooming
-    map.on('resize', updatePositions); // Update on window resize
-    map.on('moveend', updatePositions); // Final adjustment after movement ends
+  updatePositions();
+  map.on('move', updatePositions); // Update during map movement
+  map.on('zoom', updatePositions); // Update during zooming
+  map.on('resize', updatePositions); // Update on window resize
+  map.on('moveend', updatePositions); // Final adjustment after movement ends
 });
+
